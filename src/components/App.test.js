396439c341569f2f658d51b28116b48828de895b
Lunacy001 <43693807+Lunacy001@users.noreply.github.com>
2021-08-04 01:06:13 +0530

    Mocked AppContainer

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -3,6 +3,8 @@ import { render } from '@testing-library/react';
 
 import App from 'components/App';
 
+jest.mock('containers/AppContainer', () => 'mock-app-container');
+
 describe('<App />', () => {
   it('renders correctly', async () => {
     const { asFragment } = render(<App />);
